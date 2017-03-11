bd82d03eb5b12dbb7a43f317692f554f25b70268
kjendrzyca <kjendrzyca@gmail.com>
2017-03-12 00:01:51 +0100

    probably someone already did this but merging with spread operator is fun

--- money.js
+++ money.js
@@ -1,11 +1,31 @@
-let state = {}
+function moneyFactory () {
+  let state = {}
 
-export default {
-  add: (name, value) => {
-    state = {
-      ...state,
-      [name]: value
-    }
-  },
-  getAll: () => state
+  return {
+    add: (entry) => {
+      state = {
+        ...state,
+        [entry.category]: mergeCategory(state, entry)
+      }
+    },
+    getAll: () => state
+  }
 }
+
+function mergeCategory (state, entry) {
+  return {
+    ...state[entry.category],
+    [entry.name]: mergeEntry(state, entry)
+  }
+}
+
+function mergeEntry (state, entry) {
+  const alreadyExistingPrice = state[entry.category] && state[entry.category][entry.name]
+
+  if (alreadyExistingPrice) {
+    return alreadyExistingPrice + entry.price
+  }
+  return entry.price
+}
+
+export default moneyFactory
--- money.test.js
+++ money.test.js
@@ -1,15 +1,45 @@
 import test from 'tape'
-import money from './money'
+import moneyFactory from './money'
+
+const entry = (category, name, price) => ({ category, name, price })
 
 test('money', (assert) => {
-  // given
-  money.add('grocery', 5)
+  const money = moneyFactory()
+  const singleEntry = entry('grocery', 'tomato', 5)
 
-  // when
+  money.add(singleEntry)
   const actual = money.getAll()['grocery']
-  const expected = 5
+  const expected = { tomato: 5 }
 
-  // then
-  assert.equal(actual, expected, 'should add and get single value')
+  assert.deepEqual(actual, expected, 'should add single entry to the category')
+  assert.end()
+})
+
+test('money', (assert) => {
+  const money = moneyFactory()
+  money.add(entry('grocery', 'tomato', 5))
+  money.add(entry('grocery', 'potato', 10))
+
+  const actual = money.getAll()['grocery']
+  const expected = {
+    tomato: 5,
+    potato: 10
+  }
+
+  assert.deepEqual(actual, expected, 'should add two single entries to the same category')
+  assert.end()
+})
+
+test('money', (assert) => {
+  const money = moneyFactory()
+  money.add(entry('grocery', 'kiwi', 5))
+  money.add(entry('grocery', 'kiwi', 12))
+
+  const actual = money.getAll()['grocery']
+  const expected = {
+    kiwi: 17
+  }
+
+  assert.deepEqual(actual, expected, 'should sum two entries with the same name')
   assert.end()
 })
