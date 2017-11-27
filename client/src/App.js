c0964f347011fee3185c8fc86222683cc393880b
kjendrzyca <kjendrzyca@gmail.com>
2017-11-27 07:32:23 +0100

    Bootstrapped client app

---
+++ client/src/App.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react';
+import { Collapse, Navbar, NavbarToggler, NavbarBrand, Nav, NavItem, NavLink } from 'reactstrap';
+import './App.css';
+
+class App extends Component {
+  state = {
+    isOpen: false
+  }
+
+  toggle = () => {
+    this.setState({
+      isOpen: !this.state.isOpen
+    });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Navbar
+          light
+          expand={false}
+          sticky="top"
+        >
+          <NavbarBrand href="/">The Money</NavbarBrand>
+          <NavbarToggler onClick={this.toggle} />
+          <Collapse isOpen={this.state.isOpen} navbar>
+            <Nav className="ml-auto" navbar>
+              <NavItem>
+                <NavLink href="#">By Category</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink href="#">By Type</NavLink>
+              </NavItem>
+            </Nav>
+          </Collapse>
+        </Navbar>
+      </div>
+    );
+  }
+}
+
+export default App;
